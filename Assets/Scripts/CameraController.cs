7609e2790935b6faccfc0d6ec6d98e429f45e843
SoulTracK <soul.trackx@gmail.com>
2020-07-21 19:53:37 +0200

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public void LookAtTrget() 
+    {
+        Vector3 lookDirection = objectFollow.position - transform.position;
+        Quaternion rot = Quaternion.LookRotation(lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation,
+                                             rot,
+                                             lookSpeed * Time.deltaTime);
+    }
+
+    public void MoveToTarget()
+    {
+        Vector3 targetPos = objectFollow.position +
+            objectFollow.forward * offset.z +
+            objectFollow.right * offset.x +
+            objectFollow.up * offset.y;
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+    }
+
+    public void FixedUpdate()
+    {
+        LookAtTrget();
+        MoveToTarget();
+    }
+    public Transform objectFollow;
+    public Vector3 offset;
+    public float followSpeed = 10;
+    public float lookSpeed = 10;
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public void GetInput() 
+    {
+        mHorizontalInput = Input.GetAxis("Horizontal");
+        mVerticalInput = Input.GetAxis("Vertical");
+    }
+
+    public void Steer()
+    {
+        mSteeringAngle = maxSteerAngle * mHorizontalInput;
+        frontDriverWheel.steerAngle = mSteeringAngle;
+        frontPassangerWheel.steerAngle = mSteeringAngle;
+    }
+
+    public void Accelerate()
+    {
+        if (Input.GetKey("s") || Input.GetKey("down"))
+        {
+            frontDriverWheel.motorTorque = mVerticalInput * (motorForce / 2);
+            frontPassangerWheel.motorTorque = mVerticalInput * (motorForce / 2);
+
+            Debug.Log("mamasita linda");
+        }
+
+        if(Input.GetKey("space")){
+            frontDriverWheel.motorTorque = 0;
+            frontPassangerWheel.motorTorque = 0;
+        }
+
+        else {
+            frontDriverWheel.motorTorque = mVerticalInput * motorForce;
+            frontPassangerWheel.motorTorque = mVerticalInput * motorForce;
+        }
+    }
+
+    public void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontPassangerWheel, frontPassangerTransform);
+        UpdateWheelPose(frontDriverWheel, frontDriverTransform);
+        UpdateWheelPose(backPassangerWheel, backPassangerTransform);
+        UpdateWheelPose(backDriverWheel, backDriverTransform);
+    }
+
+    private void UpdateWheelPose(WheelCollider collider, Transform transform) 
+    {
+        Vector3 pos = transform.position;
+        Quaternion quat = transform.rotation;
+
+        collider.GetWorldPose(out pos, out quat);
+        
+        transform.position = pos;
+        transform.rotation = quat;
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+            
+    }
+
+    private float mHorizontalInput;
+    private float mVerticalInput;
+    private float mSteeringAngle;
+
+    public WheelCollider frontDriverWheel, frontPassangerWheel;
+    public WheelCollider backDriverWheel, backPassangerWheel;
+
+    public Transform frontDriverTransform, frontPassangerTransform;
+    public Transform backDriverTransform, backPassangerTransform;
+
+    public float maxSteerAngle = 30;
+    public float motorForce = 50;
+
+
+}
